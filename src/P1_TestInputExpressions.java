87d5f65f829af4148479525fa1004df7ee5b1e01
Paul Gross <pgross@maryville.edu>
2018-01-30 09:30:16 -0600

    Initial commit

---
+++ src/P1_TestInputExpressions.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+	ISYS 320
+	Name(s):
+	Date: 
+*/
+
+/*
+ Your expected results after the input is provided:
+ 
+ 1.
+ 2.
+ 3. 
+ 4.
+ 
+ 5.
+ 6.
+ 7.
+ 8.
+  
+ */
+
+public class P1_TestInputExpressions {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in); 
+		System.out.print("How much money do you have? "); 
+		double money = console.nextDouble(); 
+
+	}
+
+}
---
+++ src/P3_EchoChamber.java
@@ -0,0 +1,13 @@
+/*
+	ISYS 320
+	Name(s):
+	Date: 
+*/
+
+public class P3_EchoChamber {
+
+	public static void main(String[] args) {
+
+	}
+
+}
