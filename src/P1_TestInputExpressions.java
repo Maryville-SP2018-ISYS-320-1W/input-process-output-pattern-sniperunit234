92dacc50ad48285f83ee61ac19b145d74eeead0a
sniperunit234 <37313753+sniperunit234@users.noreply.github.com>
2018-03-31 16:32:34 -0400

    error
    i struggled with this and nothin produced correctly for me

--- src/P1_TestInputExpressions.java
+++ src/P1_TestInputExpressions.java
@@ -9,23 +9,23 @@ import java.util.Scanner;
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. $69 it will double in value
+ 2. 6
+ 3. $50
+ 4. million twice
  
- 5.
- 6.
- 7.
- 8.
+ 5. 250,000
+ 6. 0
+ 7. none
+ 8. 1290
   
- */
+ /* i belived i struggle with this my answers eithers didnt run or just repeated it self */
 
 public class P1_TestInputExpressions {
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in); 
-		System.out.print("How much money do you have? "); 
+		System.out.print(" How much money do you have? "); 
 		double money = console.nextDouble(); 
 
 	}
